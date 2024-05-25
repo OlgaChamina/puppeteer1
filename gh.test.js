c687c58ecf3d43ff496f5a4bc6e8359d68acc940
OlgaCh <Chamina51@mail.ru>
2024-05-25 15:34:12 +0300

    second commit

--- gh.test.js
+++ gh.test.js
@@ -13,7 +13,6 @@ describe('Github page tests', () => {
         await page.goto('https://github.com/team');
     });
     test("The h1 header content'", async () => {
-        jest.setTimeout(60000);
         const firstLink = await page.$('header div div a');
         await firstLink.click();
         await page.waitForSelector('h1');
@@ -21,18 +20,16 @@ describe('Github page tests', () => {
         expect(title2).toEqual(
             'GitHub for teams · Build like the best teams on the planet · GitHub'
         );
-    });
+    }, 60000);
 
     test('The first link attribute', async () => {
-        jest.setTimeout(60000);
         const actual = await page.$eval('a', (link) =>
             link.getAttribute('href')
         );
         expect(actual).toEqual('#start-of-content');
-    });
+    }, 60000);
 
     test('The page contains Sign in button', async () => {
-        jest.setTimeout(60000);
         const btnSelector = '.btn-large-mktg.btn-mktg';
         await page.waitForSelector(btnSelector, {
             visible: true
@@ -42,7 +39,7 @@ describe('Github page tests', () => {
             (link) => link.textContent
         );
         expect(actual).toContain('Get started with Team');
-    });
+    }, 60000);
 });
 
 test("check the headers on other pages1'", async () => {
@@ -50,27 +47,27 @@ test("check the headers on other pages1'", async () => {
     //await page.evaluate(() => {
     //    debugger;
     //});
-    jest.setTimeout(60000);
+
     const firstLink = await page.$(
         '.HeaderMenu-link.no-underline.px-0.px-lg-2.py-3.py-lg-2.d-block.d-lg-inline-block'
     );
     await firstLink.click();
     const title2 = await page.title();
     expect(title2).toEqual('GitHub: Let’s build from here · GitHub');
-});
+}, 60000);
+
 test("check the headers on other pages2'", async () => {
     await page.goto('https://github.com/pricing');
-    jest.setTimeout(60000);
     const firstLink = await page.$("h1[class='h2-mktg']");
     await firstLink.click();
     const title2 = await page.title();
     expect(title2).toEqual('Pricing · Plans for every developer · GitHub');
-});
+}, 60000);
+
 test("check the headers on other pages3'", async () => {
     await page.goto('https://github.com/login');
-    jest.setTimeout(60000);
     const firstLink = await page.$("div[class='auth-form-header p-0'] h1");
     await firstLink.click();
     const title2 = await page.title();
     expect(title2).toEqual('Sign in to GitHub · GitHub');
-});
+}, 60000);
